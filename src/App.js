5e3bc366cd0d95afd343eb70ea82f0ef5c7abad5
Olha Orach <olhaorach2002@gmail.com>
2022-08-12 12:45:35 +0100

    Did changes in App.js.

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import { SignIn } from "./components/Sign-In/Sign-In";
 import { SignUp } from "./components/Sign-Up/Sign-Up";
 import "./App.css";
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" exact element={<SignIn />} />
         <Route path="/signup" element={<SignUp />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
